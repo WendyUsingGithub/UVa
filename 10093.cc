2bba083b19ddedf36da4830075d783942315cb48
wendy <cutebitterblue@gmail.com>
2021-07-31 19:37:30 +0800

    UVa 10093 An Easy Problem

--- 10093.cc
+++ 10093.cc
@@ -1,31 +1,14 @@
-/* UVA 10093 An Easy Problem */
-/* 2021/5/2 Wendy */
+/* UVa 10093 An Easy Problem */
+/* 2021/7/31 Wendy */
 
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-// Input
-// Each line in the input file will contain an integer (as defined in mathematics) number of any integer
-// base (2..62). You will have to determine what is the smallest possible base of that number for the given
-// conditions. No invalid number will be given as input.
-// Output
-// If number with such condition is not possible output the line ‘such number is impossible!’ For each
-// line of input there will be only a single line of output. The output will always be in decimal number
-// system.
-// Sample Input
-// 3
-// 5
-// A
-// Sample Output
-// 4
-// 6
-// 11
-
-/* input is a string
-   imply that this might be a big number problem */
-
 int isMultiple(string str, int base);
+
+/* this is a big number problem */
+/* testcase includes negative input */
 
 int main()
 {
@@ -33,11 +16,7 @@ int main()
     string strBase = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
     int strLen;
     int minBase = 2;
-    // 10+26+26 = 62
     int N;
-
-    // (0..9 and A..Z and a..z
-    //     10   11  36    37  62
 
     while(cin >> strR) {
         strLen = strR.length();
@@ -45,7 +24,7 @@ int main()
 
         for(int i = 0; i < strLen; i++) {
             strR[i] = strBase.find(strR[i]);
-            if(strR[i] == -1) strR[i] = 0;
+            if(strR[i] == -1) strR[i] = 0; /* for negative input */
         }
 
         for(int i = 0; i < strLen; i++) {
@@ -55,7 +34,7 @@ int main()
         }
 
         for(N = minBase; N <= 62; N++) {
-            if(isMultiple(strR, N)) {
+            if(isMultiple(strR, N)) { /* strR is a N base big number */
                 break;
             }
         }
@@ -63,6 +42,8 @@ int main()
         if(N == 63) cout << "such number is impossible!" << endl;
         else cout << N << endl; 
     }
+
+    return 0;
 }
 
 int isMultiple(string str, int N) 
